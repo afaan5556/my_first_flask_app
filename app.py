3e447f1610eafa5c55477978b95e5bc9b25c7abb
Afaan Naqvi <afaan.naqvi@gmail.com>
2021-09-17 10:42:54 +0200

    Get CRUD Update working. Flake8 and refactoring

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, make_response, request, jsonify, redirect, flash, url_for
+from flask import Flask, render_template, make_response, request, redirect, flash, url_for
 from models.visitor import VisitorModel
-# from flask_restful import Api
-from flask_jwt import JWT
 from db import db
-from security import authenticate, identity
+# from security import authenticate, identity
+# from flask_restful import Api
+# from flask_jwt import JWT
 
 
 app = Flask(__name__)
@@ -16,12 +16,14 @@ app.secret_key = 'my_first_flask_app_key'
 
 # Ask SQL Alchemy to create database and tables
 # Has to run before first request
+
+
 @app.before_first_request
 def create_tables():
     db.create_all()
 
-
-jwt = JWT(app, authenticate, identity)
+# User authorization using jwt turned off
+# jwt = JWT(app, authenticate, identity)
 
 
 # Index is long form scrolling splash page
@@ -43,17 +45,18 @@ def visitors():
     visitors = [visitor.json() for visitor in VisitorModel.query.all()]
     return make_response(render_template('visitors.html', visitors=visitors), 200, headers)
 
+
 # Visitor CRUD routes
 @app.route('/visitor/add', methods=['POST'])
 def add():
-    data = {'name': request.form['name'],
-    'first_name': request.form['first_name'],
-    'last_name': request.form['last_name'],
-    'email': request.form['email'],
-    'phone': request.form['phone']
-    }
+    data = {'user_name': request.form['user_name'],
+            'first_name': request.form['first_name'],
+            'last_name': request.form['last_name'],
+            'email': request.form['email'],
+            'phone': request.form['phone']
+            }
 
-    visitor = VisitorModel(data['name'], data['first_name'], data['last_name'], data['email'], data['phone'])
+    visitor = VisitorModel(data['user_name'], data['first_name'], data['last_name'], data['email'], data['phone'])
 
     try:
         visitor.save_to_db()
@@ -63,23 +66,49 @@ def add():
     return redirect(url_for('visitors'), code=302)
 
 
-@app.route('/visitor/<name>/delete', methods=('GET', 'POST'))
-def delete(name):
-    visitor = VisitorModel.find_by_name(name)
+@app.route('/visitor/<user_name>/edit', methods=('GET', 'POST'))
+def edit(user_name):
+
+    data = {'first_name': request.form['first_name'],
+            'last_name': request.form['last_name'],
+            'email': request.form['email'],
+            'phone': request.form['phone']
+            }
+
+    visitor = VisitorModel.find_by_user_name(user_name)
 
     if not visitor:
-        flash('Visitor with name {} was not found!'.format(name))
+        visitor = VisitorModel(user_name, **data)
+    else:
+        visitor.first_name = data['first_name']
+        visitor.last_name = data['last_name']
+        visitor.email = data['email']
+        visitor.phone = data['phone']
+
+    try:
+        visitor.save_to_db()
+    except:
+        flash('Problem editing visitor with user_name {}!'.format(user_name))
+        return redirect("/visitors", code=500)
+    return redirect(url_for('visitors'))
+
+
+@app.route('/visitor/<user_name>/delete', methods=('GET', 'POST'))
+def delete(user_name):
+    visitor = VisitorModel.find_by_user_name(user_name)
+
+    if not visitor:
+        flash('Visitor with user_name {} was not found!'.format(user_name))
         return redirect(url_for('visitors'), code=404)
     else:
         try:
             visitor.delete_from_db()
         except:
-            flash('Problem deleting visitor with name {}!'.format(name))
+            flash('Problem deleting visitor with user_name {}!'.format(user_name))
             return redirect("/visitors", code=500)
 
-    flash('Visitor {} was successfully deleted!'.format(name))
+    flash('Visitor {} was successfully deleted!'.format(user_name))
     return redirect(url_for('visitors'))
-
 
 
 if __name__ == "__main__":
--- models/visitor.py
+++ models/visitor.py
@@ -3,22 +3,22 @@ from db import db
 class VisitorModel(db.Model):
     __tablename__ = 'visitors'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
+    # id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(80), primary_key=True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     email = db.Column(db.String(80))
     phone = db.Column(db.String(80))
 
-    def __init__(self, name, first_name, last_name, email, phone):
-        self.name = name
+    def __init__(self, user_name, first_name, last_name, email, phone):
+        self.user_name = user_name
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.phone = phone
 
     def json(self):
-        return {'name': self.name,
+        return {'user_name': self.user_name,
                 'first_name': self.first_name,
                 'last_name': self.last_name,
                 'email': self.email,
@@ -26,8 +26,8 @@ class VisitorModel(db.Model):
                 }
 
     @classmethod
-    def find_by_name(cls, name):
-        return cls.query.filter_by(name=name).first()
+    def find_by_user_name(cls, user_name):
+        return cls.query.filter_by(user_name=user_name).first()
 
 
     def save_to_db(self):
